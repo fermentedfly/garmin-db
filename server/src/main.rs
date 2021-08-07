7d76e836d586cb4c40655a0021bd38e555cb240f
Manuel Seeböck <seeboeck@pjm.co.at>
2021-08-07 09:47:07 +0200

    read activities into vec

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,25 +1,34 @@
 pub mod garmin;
 
+use std::env;
 use std::error::Error;
-use std::io;
 use std::process;
 
-fn read_csv() -> Result<(), Box<dyn Error>> {
-    let mut rdr = csv::ReaderBuilder::new()
-        .delimiter(b',')
-        .from_reader(io::stdin());
-    for result in rdr.deserialize() {
-        // Notice that we need to provide a type hint for automatic
-        // deserialization.
-        let record: garmin::Activity = result?;
-        println!("{:?}", record);
-    }
-    Ok(())
+fn read_csv(path: &str) -> Result<Vec<garmin::Activity>, Box<dyn Error>> {
+    let mut reader = csv::ReaderBuilder::new().delimiter(b',').from_path(path)?;
+
+    let records: Result<Vec<garmin::Activity>, _> = reader.deserialize().into_iter().collect();
+    Ok(records?)
 }
 
 fn main() {
-    if let Err(err) = read_csv() {
-        println!("error running example: {}", err);
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("error missing csv file name");
         process::exit(1);
+    }
+
+    let filename = &args[1];
+
+    match read_csv(filename) {
+        Ok(data) => {
+            for d in data {
+                println!("{:?}", d);
+            }
+        }
+        Err(err) => {
+            println!("error decoding csv: {}", err);
+        }
     }
 }
